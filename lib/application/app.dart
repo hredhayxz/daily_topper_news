e8ab18c30e97a136d36bd0d2263e1f847c57e1a5
Md Alhaz Mondal Hredhay <60058949+hredhayxz@users.noreply.github.com>
2024-10-05 16:57:21 +0600

    flutter_screenutil has been added for responsive design

--- lib/application/app.dart
+++ lib/application/app.dart
@@ -2,6 +2,7 @@ import 'package:daily_topper_news/application/app_bindings.dart';
 import 'package:daily_topper_news/application/theme_manager.dart';
 import 'package:daily_topper_news/presentation/screens/splash_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 
 class DailyTopper extends StatelessWidget {
@@ -9,12 +10,20 @@ class DailyTopper extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Daily Topper',
-      theme: ThemeManager.getAppTheme(),
-      initialBinding: AppBindings(),
-      home: const SplashScreen(),
-    );
+    return ScreenUtilInit(
+        designSize: MediaQuery.of(context).size.width > 600
+            ? const Size(834, 1194) // Tablet design size
+            : const Size(430, 932), // Mobile design size
+        minTextAdapt: true,
+        splitScreenMode: true,
+        builder: (context, child) {
+          return GetMaterialApp(
+            debugShowCheckedModeBanner: false,
+            title: 'Daily Topper',
+            theme: ThemeManager.getAppTheme(),
+            initialBinding: AppBindings(),
+            home: const SplashScreen(),
+          );
+        });
   }
 }
